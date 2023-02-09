82bfc626518bd26995863a2a4eaac44cd8af8a2e
Tyson Tran <tysontran2@gmail.com>
2023-02-08 19:46:57 -0800

    Set up code organization

---
+++ run.py
@@ -0,0 +1,30 @@
+
+import sys
+import os
+import json
+
+#Import functions from src
+
+
+def main(targets):
+
+	if "test" in targets:
+		...
+
+	if "all" in targets:
+		...
+
+	if "data" in targets:
+		...
+
+	if "features" in targets:
+		...
+
+	if "model" in targets:
+		...
+
+	return
+
+if __name__ == "__main__":
+	targets = sys.argv[1:]
+	main(targets)
